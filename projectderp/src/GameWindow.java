66b12110b8c82a8c45fffe18d4ddded916421426
c3mjarvi <mikael.jarvinen@cs.tamk.fi>
2015-02-25 16:40:52 +0200

    Improves the closing of the windows.
    Adds question-dialogue when closing GameWindow, where you can choose do
    you want to exit completely, return to MenuWindow or to cancel the
    action. Hidden MenuWindow now closes when closing the game and 
    becomes visible when closing just the GameWindow.
    
    Also, black square removed from the canvas.

--- projectderp/src/GameWindow.java
+++ projectderp/src/GameWindow.java
@@ -6,9 +6,13 @@
  * @since 1.7
  */
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 class GameWindow extends JFrame {
     
@@ -48,9 +52,18 @@ class GameWindow extends JFrame {
     private Player player;
     
     /**
+     * The parent window. It needs to be closed in code because it's hidden 
+     * when GameWindow is visible and it doesn't automatically close when
+     * GameWindow is closed. 
+     */
+    private MenuWindow menu;
+    
+    /**
      * Constructs the window used in the game engine.
      */
-    public GameWindow() {
+    public GameWindow(final MenuWindow menu) {
+    	
+    	this.menu = menu;
         windowWidth = 600;
         windowHeight = 500;
         player = new Player(Toolkit.getDefaultToolkit().createImage("res/derpvivor.png"), 64, 64);
@@ -73,7 +86,31 @@ class GameWindow extends JFrame {
         
         setTitle("Game Window");
         pack();
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        // do nothing on close because there's a custom closing operation
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        
+        // custom closing operation
+        WindowListener exitListener = new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+            	
+            	Object[] options = {"Yes", "No", "To menu"};
+            	
+                int confirm = JOptionPane.showOptionDialog(null, 
+                										   "Are You Sure to Close Application?", "Exit Confirmation", 
+                										   JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+                if (confirm == 0) {
+                	System.exit(0);
+             	
+                } else if (confirm == 2) {
+                	menu.setVisible(true);
+                	dispose();
+                }
+            }
+        };
+        
+        addWindowListener(exitListener);
+        
         setVisible(true);
     }
 }
